e36fdc6be794881371b8c515b0853aea8060d213
James Karnes <karnes.james@gmail.com>
2021-02-06 17:50:05 -0400

    First Ideas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Grid from '@material-ui/core/Grid'
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Grid container spacing={1}>
+      <Grid className='index-picture2' item xs={6}><Link to="/diving/"><img src="https://i.ibb.co/qmX8Wwc/20190501-093335.jpg" alt=""/></Link><p>Diving</p> </Grid>
+      <Grid className='index-picture2' item xs={6}><Link to="/watersports/"><img src="https://i.ibb.co/fkCx3L6/franklin-aboutus2.jpg" alt=""/></Link><p>Water Sports</p></Grid>
+      <Grid className='index-picture' item xs={6}><Link to="/boats/"><img src="https://i.ibb.co/Gc9Ynwg/JHO-0831-Editar.jpg" alt=""/></Link><p>Boat Rentals</p> </Grid>
+      <Grid className='index-picture' item xs={6}><Link to="/snorkeling/"><img src="https://i.ibb.co/cDVsNzx/IMG-0466.jpg" alt=""/></Link> <p>Snorkeling</p> </Grid>
+    </Grid>
+
   </Layout>
 )
 
